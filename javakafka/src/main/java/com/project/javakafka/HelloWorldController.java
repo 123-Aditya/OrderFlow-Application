7eaf91a3732ded99b7160847c622ccb0f8bebb16
123-Aditya <mraditya345@gmail.com>
2025-10-22 23:25:30 +0530

    Adding Controller and doecker-compose.yml

--- javakafka/src/main/java/com/project/javakafka/HelloWorldController.java
+++ javakafka/src/main/java/com/project/javakafka/HelloWorldController.java
@@ -1,8 +1,23 @@
 package com.project.javakafka;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private final KafkaProducerService producerService;
+	
+	
+	public HelloWorldController(KafkaProducerService producerService) {
+		this.producerService = producerService;
+	}
+	
 
+	@RequestMapping("/send")
+	public String sendMessage(@RequestParam("message") String message) {
+		producerService.sendMessage(message);
+		return "Message sent to Kafka topic: " + message;
+	}
 }
