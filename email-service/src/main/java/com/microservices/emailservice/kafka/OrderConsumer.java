c307373b6ffe2252b3e1f8a1f85d9f853d87789e
123-Aditya <mraditya345@gmail.com>
2025-11-04 12:10:08 +0530

    Added logic to send email in OrderConsumer

--- email-service/src/main/java/com/microservices/emailservice/kafka/OrderConsumer.java
+++ email-service/src/main/java/com/microservices/emailservice/kafka/OrderConsumer.java
@@ -4,19 +4,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import com.microservices.basedomains.dto.OrderEvent;
+import com.microservices.emailservice.service.EmailService;
+import com.microservices.emailservice.config.EmailRecipientsConfig;
 
 @Service
 public class OrderConsumer {
 
 	private static final Logger LOG = LoggerFactory.getLogger(OrderConsumer.class);
-	
-	@KafkaListener(topics="${app.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
+
+	@Autowired
+	private EmailService emailService;
+
+	@Autowired
+	private EmailRecipientsConfig emailRecipientsConfig;
+
+	@KafkaListener(topics = "${app.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
 	public void consume(OrderEvent event) {
-		
-		LOG.info(String.format("Order event received in Stock service: ", event));
-		
-		// send an email to the customers
+		LOG.info("Order event received in Email service: {}", event);
+
+		String subject = "New Order received";
+		String message = "Order Details:\n" + event.toString();
+
+		emailService.sendEmailToAll(emailRecipientsConfig.getEmails(), subject, message);
+
+		LOG.info("Email sent to all recipients.");
 	}
 }
