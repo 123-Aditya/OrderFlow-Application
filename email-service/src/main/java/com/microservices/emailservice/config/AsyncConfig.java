6e8ed7a16713fa0519b81dd4aa3a04a614608bf7
123-Aditya <mraditya345@gmail.com>
2025-11-06 20:03:17 +0530

    Added Retry in AsyncConfig.java

--- email-service/src/main/java/com/microservices/emailservice/config/AsyncConfig.java
+++ email-service/src/main/java/com/microservices/emailservice/config/AsyncConfig.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
 import java.util.concurrent.Executor;
+import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
 @EnableAsync
+@EnableRetry
 public class AsyncConfig {
 
     @Bean(name = "emailExecutor")
