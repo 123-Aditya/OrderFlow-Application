6b9d4a0bc2bbf86c4f0b60053a1a68282edc52d3
123-Aditya <mraditya345@gmail.com>
2025-11-10 23:26:36 +0530

    Updated OrderConsumer in StockService

--- stock-service/src/main/java/com/microservices/stockservice/kafka/OrderConsumer.java
+++ stock-service/src/main/java/com/microservices/stockservice/kafka/OrderConsumer.java
@@ -4,19 +4,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.microservices.basedomains.dto.OrderEvent;
+import com.microservices.stockservice.entity.OrderEventEntity;
+import com.microservices.stockservice.repository.OrderEventRepository;
+
+import java.time.LocalDateTime;
 
 @Service
 public class OrderConsumer {
 
-	private static final Logger LOG = LoggerFactory.getLogger(OrderConsumer.class);
-	
-	@KafkaListener(topics="${app.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
-	public void consume(OrderEvent event) {
-		
-		LOG.info(String.format("Order event received in Stock service: ", event));
-		
-		// save the order event into  the database
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @Autowired
+    private OrderEventRepository orderEventRepository;
+
+    @KafkaListener(topics = "${app.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event) {
+        LOG.info("Order event received in Stock service: {}", event);
+
+        try {
+            OrderEventEntity entity = new OrderEventEntity(
+                    event.getOrder().getOrderId(),
+                    event.getOrder().getName(),
+                    event.getOrder().getQty(),
+                    event.getOrder().getPrice(),
+                    event.getStatus(),
+                    LocalDateTime.now()
+            );
+
+            orderEventRepository.save(entity);
+            LOG.info("Order event saved to database: {}", event.getOrder().getOrderId());
+        } catch (Exception e) {
+            LOG.error("Failed to save order event: {}", e.getMessage(), e);
+        }
+    }
 }
