b48750ebb215e8c5c4b2f511a5ce7095322cae35
123-Aditya <mraditya345@gmail.com>
2025-11-12 19:12:35 +0530

    Updating OrderEventRepository in  stock service

--- stock-service/src/main/java/com/microservices/stockservice/repository/OrderEventRepository.java
+++ stock-service/src/main/java/com/microservices/stockservice/repository/OrderEventRepository.java
@@ -3,6 +3,12 @@ package com.microservices.stockservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.microservices.stockservice.entity.OrderEventEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
-public interface OrderEventRepository extends JpaRepository<OrderEventEntity, Long> {}
+public interface OrderEventRepository extends JpaRepository<OrderEventEntity, Long> {
+	
+    Page<OrderEventEntity> findByStatusIgnoreCase(String status, Pageable pageable);
+
+}
